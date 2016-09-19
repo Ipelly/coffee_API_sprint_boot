0452bb84b89f958cf132988b3dffa9a0fd793717
Rafaat Hossain <rafaat123@yahoo.com>
2016-09-19 13:16:52 -0400

    setting up basic oauth2

--- src/main/java/com/demazetech/coffee/api/services/MenuItemService.java
+++ src/main/java/com/demazetech/coffee/api/services/MenuItemService.java
@@ -4,6 +4,7 @@ import com.demazetech.coffee.api.dao.MenuItemDao;
 import com.demazetech.coffee.api.objects.MenuItem;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import javax.annotation.security.RolesAllowed;
 import java.util.List;
 
 public class MenuItemService {
@@ -11,7 +12,8 @@ public class MenuItemService {
     @Autowired
     private MenuItemDao menuItemDao;
 
-    // access control and business logics will be implemented here at service level 
+    // access control and business logics will be implemented here at service level
+    @RolesAllowed("USER")
     public List<MenuItem> listMenuItems() {
         return menuItemDao.listMenuItems();
     }
