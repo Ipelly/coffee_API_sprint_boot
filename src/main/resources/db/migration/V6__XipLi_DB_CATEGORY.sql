f2b23e036d22e6a2233613b8c5ee97f19b66f786
ipeli <ipelly.bd@gmail.com>
2017-01-24 22:22:46 -0500

    Created table for category

---
+++ src/main/resources/db/migration/V6__XipLi_DB_CATEGORY.sql
@@ -0,0 +1,14 @@
+
+
+-- Create New category table
+CREATE TABLE xipli.category (
+  category_id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  description VARCHAR(255) NULL,
+  shop_id INT NOT NULL,
+  statuS VARCHAR(45) NULL,
+  PRIMARY KEY (category_id),
+  UNIQUE INDEX category_id_UNIQUE (category_id ASC)
+);
+
+
