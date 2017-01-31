c926e8c467a227025c9db5338225475050a76ab3
Iqbal Ipel <ipelly.bd@gmail.com>
2017-01-31 12:52:00 -0500

    modified the type of status

--- src/main/resources/db/migration/V6__XipLi_DB_CATEGORY.sql
+++ src/main/resources/db/migration/V6__XipLi_DB_CATEGORY.sql
@@ -1,14 +1,10 @@
-
-
--- Create New category table
 CREATE TABLE xipli.category (
   category_id INT NOT NULL AUTO_INCREMENT,
   name VARCHAR(255) NOT NULL,
   description VARCHAR(255) NULL,
-  shop_id INT NOT NULL,
-  status VARCHAR(45) NULL,
+  shop_id int(11) UNSIGNED NOT NULL,
+  status tinyint(3) UNSIGNED NOT NULL DEFAULT '1',
   PRIMARY KEY (category_id),
   UNIQUE INDEX category_id_UNIQUE (category_id ASC)
 );
-
 
